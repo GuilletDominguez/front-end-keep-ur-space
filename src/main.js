80defabbecb81641e1d84e24cfd493545b17d68c
Manuel <manueligor@mixmail.com>
2021-09-27 13:46:41 +0200

    menu navegacion activo el registro

--- src/main.js
+++ src/main.js
@@ -5,4 +5,5 @@ import store from './store'
 window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js")
 import "./assets/css/portal.css";
 import "./assets/js/app.js";
+
 createApp(App).use(store).use(router).mount('#app')

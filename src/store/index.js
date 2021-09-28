8481a7a7173cb94c7744e3de9c8dd390f7335d8e
rosario gamer <82204530+rosariogamer@users.noreply.github.com>
2021-09-28 09:11:09 +0200

    cambios login

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,18 @@ import { createStore } from 'vuex'
 import axios from 'axios'
 
 
+
 export default createStore({
   state: {
     currentRequest:[],
+    currentUser:[],
   },
   mutations: {
     setCurrentRequest(state,payload){
       state.currentRequest = payload
+    },
+    setCurrentUser(state,payload){
+      state.currentUser = payload
     }
   },
   actions: {
@@ -38,6 +43,45 @@ export default createStore({
          }
    
        },
+       async login ({commit},data){
+
+
+        try{
+     
+         
+         const response = await fetch('http://localhost:8000/api/login',{
+           method: 'POST',
+           body: JSON.stringify(data),
+           headers: {
+             Accept: 'application/json',
+             'Content-type': 'application/json',
+             
+         }
+         })
+         const res = await response.json()
+         console.log(res)
+
+        localStorage.setItem("user",res.user.name);
+        localStorage.setItem("token",res.token)
+
+
+
+
+
+         commit('setCurrentUser', res)
+        
+          window.location.assign("/")
+         
+     
+        }
+           catch(err) {
+             //  this.$router.replace({ path: 'Login' })
+             console.error(err)
+           }
+     
+         },
+       
+
   },
   modules: {
   }

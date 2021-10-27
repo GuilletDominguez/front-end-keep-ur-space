b47e562dbbf8d1c27ea57e8a7c37704c3c10792a
AntViz <82447009+AntViz@users.noreply.github.com>
2021-10-27 15:37:00 +0200

    Creación del calendario

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Historico from "../views/Historico.vue";
 import UnreadRequest from '../views/UnreadRequest.vue'
 import Calendar from '../views/Calendar.vue'
 import ListaUsuario from "../views/ListaUsuario.vue";
+import Agenda from "../views/Agenda.vue";
 
 
 
@@ -97,6 +98,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Calendar.vue')
+
+  },
+  {
+    path: '/agenda',
+    name: 'Agenda',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Agenda.vue')
 
   },
   {

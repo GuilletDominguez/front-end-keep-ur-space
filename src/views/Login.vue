5f6df8fa9ed5ff485abdf0b5785f7abf361ed6e0
rosario gamer <82204530+rosariogamer@users.noreply.github.com>
2021-10-29 13:25:05 +0200

    cambios de logo

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,6 @@
 <template>
   <div class="app app-login p-0">
     <div class="row g-0 app-auth-wrapper">
-
       <div
         style="background-color: ; url(../assets/images/background/background-2.jpg);"
         class="col-12 col-md-7 col-lg-6 auth-main-col text-center p-5"
@@ -19,7 +18,6 @@
             </div>
             <h2 class="auth-heading text-center mb-5">RESERVA DE SALAS</h2>
             <div class="auth-form-container text-start">
-
               <form @submit.prevent="login" class="auth-form login-form">
                 <div class="email mb-3">
                   <label class="sr-only" for="email">Email</label>
@@ -64,18 +62,15 @@
                   </button>
                 </div>
               </form>
-
             </div>
             <!--//auth-form-container-->
           </div>
           <!--//auth-body-->
 
           <footer class="app-auth-footer">
-
             <div class="container text-center py-3">
               <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
             </div>
-
           </footer>
           <!--//app-auth-footer-->
         </div>
@@ -84,20 +79,15 @@
       <!--//auth-main-col-->
       <div class="col-12 col-md-5 col-lg-6 h-100 auth-background-col">
         <div class="auth-background-holder"></div>
-
-
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
-
-import { useStore } from 'vuex'
-import router from '../router';
+import { useStore } from "vuex";
+import router from "../router";
 export default {
-
   setup() {
     const user = {
       email: "",
@@ -109,7 +99,7 @@ export default {
     const login = (a) => {
       console.log(user);
       store.dispatch("login", user);
-      router.push({name: 'Home'})
+      router.push({ name: "Home" });
     };
 
     return {
@@ -124,7 +114,7 @@ export default {
 .app-login .auth-background-holder[data-v-26084dc2] {
   background: url(../assets/images/background/background-0.jpg) no-repeat center
     center !important;
-width: 100% !important;
+  width: 100% !important;
 }
 
 .app-auth-wrapper .app-logo .logo-icon {
@@ -135,8 +125,7 @@ width: 100% !important;
   width: 800px !important;
   height: 80px !important;
   background-color: white !important;
-
-
+}
 .form-control {
   border: 1px solid black;
 }
@@ -151,5 +140,4 @@ textarea {
 .app-auth-wrapper {
   height: 115vh;
 }
-
 </style>
